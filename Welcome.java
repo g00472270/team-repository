ac0962c8ebf6b55bb2a62f3171e23431b9c4dbf8
g00472270 <g00472270@atu.ie>
2024-10-25 11:48:57 +0100

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,8 @@
 // for github lab 2
+
 public class Welcome{
-public static void main(String[] args){
-System.out.println("Welcome to Github lab 2!");
-System.out.println("lpB now collaborating");
-}
+  public static void main(String[] args){
+    System.out.println("Welcome to Github lab 2!");
+    System.out.println("lpA making changes");
+  }
 }
